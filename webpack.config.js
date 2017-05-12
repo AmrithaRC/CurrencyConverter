f2322cc78f0e7f6724efcbcd584fef1b2b68b10a
Am <Am@0am@gmail.com>
2017-05-12 17:28:56 +0530

    kept as single bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 var webpack = require('webpack');
 module.exports= {
 	entry:{
-			app:'./app/app.js',
-			vendor:['./node_modules/angular']
+			app:['./app/app.js','./node_modules/angular'],
 		},
 	output:{
 		path: './bin',
 		filename: 'app.bundle.js'
 	},
-	 plugins: [
-	           new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
-	       ]
+//	 plugins: [
+//	           new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+//	       ]
 }
